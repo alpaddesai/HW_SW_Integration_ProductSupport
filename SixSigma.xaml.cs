af8d66e1e84615f21922f0283bab35fe426959c2
unknown <alpa.gupta@gmail.com>
2019-07-09 22:13:19 -0400

    initial commmit

---
+++ SixSigma.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QualityAssurance
+{
+    /// <summary>
+    /// Interaction logic for SixSigma.xaml
+    /// </summary>
+    public partial class SixSigma : Window
+    {
+        public SixSigma()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
